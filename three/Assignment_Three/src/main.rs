f1153b90a8a493c7082dd19f2e95d4c6c807c288
karanborate29 <karanborate29@gmail.com>
2020-08-24 15:58:28 +0530

    Adding second and third assignment

--- three/Assignment_Three/src/main.rs
+++ three/Assignment_Three/src/main.rs
@@ -1,25 +1,21 @@
 //assignment_three
+
 use std::io::{self, Write};
 
 fn main(){
 
-    let arr = [
-        1, 10, 20, 47, 59, 63, 75, 88, 99,
-        107, 120, 133, 155, 162, 176, 188,
-        199, 200, 210, 222
-    ];
+    let arr = [1, 10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155, 162, 176, 188, 199, 200, 210, 222];
     let target: i32 = 47;
 
-    linearsearch(&arr,&target);
-    binarysearch(&arr, arr.len(), &target);
+    linearsearch(arr,target);   //Q1(a)_Calling Linear_Search
+    binarysearch(arr, target);  //Q1(b)_Calling Binary_Search
 
-    let mut nums = vec![133, 10, 210, 59, 47, 63, 75, 88, 99
-                        , 120, 1, 155, 162, 176, 222,
-                        199, 200, 20, 107];
+    let mut nums = vec![133, 10, 210, 59, 47, 63, 75, 88, 99, 120, 1, 155, 162, 176, 222, 199, 200, 20, 107];
 
     let l = nums.len();
-    merge_sort(&mut nums, 0, l - 1);
-    println!("Implementing Merge Sort");
+    merge_sort(&mut nums, 0, l - 1);    //Q2_Merge_Sort
+
+    println!("Displaying Sorted Array");
     for f in nums {
         print!("{} ", f);
     }
@@ -27,68 +23,67 @@ fn main(){
     io::stdout().flush().unwrap();
 
     let tup=(04,1999);
-    leap(tup);
+    leap(tup);  //Q3_Calling Leap Year
 }
 
-pub fn linearsearch(arr: &[i32],target_value:&i32) {
+pub fn linearsearch(arr: [i32;20],target_value:i32) {
+        let size = arr.len();
+        linearsearchrecursive(arr, 0, size as i32, target_value);
+}
 
-    println!("In Linear Search");
-    for  val in arr{
-            if val==target_value {
-                println!("{} is present",target_value);
-        }
-
+pub fn linearsearchrecursive(arr: [i32;20], position:i32, length:i32, target_value:i32) {
+    let my_size = length-1;
+    if my_size < -1 {
+        println!("In liner search, found empty array");
     }
+    if arr[position as usize] == target_value {
+        println!("In liner search, {} at position {}",target_value, (position+1));
+        return ();
+    }
+    if arr.len() != my_size as usize && arr[mySize as usize] == target_value {
+        println!("In liner search, {} at position {}",target_value, (my_size+1));
+        return ();
+    }
+    let size = arr.len()-1;
+    linearsearchrecursive(arr, position+1, (size - 1) as i32, target_value);
+}
+
+pub fn binarysearch(arr: [i32;20],target_value:i32){
+    let size = arr.len();
+    binarysearchrecursive(arr, 0, size as i32, target_value);
 
 }
 
+pub fn binarysearchrecursive(arr: [i32;20], start:i32, len: i32, target_value: i32) {
+    let mut low: i32 = start ;
+    let mut high: i32 = len - 1;
 
-
-pub fn binarysearch(a: &[i32], len: usize, target_value: &i32) {
-    println!("");
-    println!("In Binary Search");
-    let mut low: i8 = 0;
-    let mut high: i8 = len as i8 - 1;
-
-    while low <= high {
         let mid = ((high - low) / 2) + low;
         let mid_index = mid as usize;
-        let val = &a[mid_index];
+        let val = arr[mid_index];
         if val == target_value {
-            println!("{} found at index {}", target_value, mid_index);
+            println!("In Binary Search, {} found at index {}", target_value, mid_index);
             println!(" ");
-            break;
+            return();
         }
 
         // Search values that are greater than val - to right of current mid_index
         if val < target_value {
             low = mid + 1;
+            binarysearchrecursive(arr, low , arr.len() as i32, target_value);
         }
 
         // Search values that are less than val - to the left of current mid_index
         if val > target_value {
             high = mid - 1;
+            binarysearchrecursive(arr, 0,  high, target_value);
         }
-    }
+
 
 }
 
 
-/*use std::io::{self, Write};
-fn main() {
-    let mut nums = vec![133, 10, 210, 59, 47, 63, 75, 88, 99
-                        , 120, 1, 155, 162, 176, 222,
-                        199, 200, 20, 107];
 
-    let l = nums.len();
-    merge_sort(&mut nums, 0, l - 1);
-    println!("Implementing Merge Sort");
-    for f in nums {
-        print!("{} ", f);
-    }
-    io::stdout().flush().unwrap();
-}
-*/
 pub fn merge_sort(a: &mut Vec<i32>, b: usize, e: usize) {
     if b < e {
         let m = (b+e)/2;
@@ -97,6 +92,7 @@ pub fn merge_sort(a: &mut Vec<i32>, b: usize, e: usize) {
         merge(a, b, m, e);
     }
 }
+
 fn merge(a: &mut Vec<i32>, b: usize, m:usize, e:usize) {
     let mut left = a[b..m+1].to_vec();
     let mut right = a[m+1..e+1].to_vec();
--- two/Assignment_two/src/main.rs
+++ two/Assignment_two/src/main.rs
@@ -1,23 +1,25 @@
+//assignment_two
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!");  //Q2
     println!("");
     fibo();
 }
 
-pub fn fibo(){
+pub fn fibo(){  //Q3
 
     let mut i =0;
     let mut a=0;
     let mut b=1;
     let mut c;
-    //let mut x=100;
+
 
     while i<10 {
 
         if i>1 {
-            c=b;//1//1//2//3
-            b=b+a;//1//2//3//5
-            a=c;//1//1//2//3
+            c=b;    //1//1//2//3
+            b=b+a;  //1//2//3//5
+            a=c;    //1//1//2//3
 
             println!("{}",b);
             i=i+1;
